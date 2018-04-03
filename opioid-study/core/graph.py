fb6e5fbe4ee955b45a37f0ab64b258437c78cd8b
Brian <VanKrieg@users.noreply.github.com>
2018-04-03 13:16:37 -0500

    Add files via upload
    changed graph and parser

--- opioid-study/core/graph.py
+++ opioid-study/core/graph.py
@@ -1,37 +1,26 @@
-from core.comment import Comment
-
-class Graph(object):
-    def __init__(self):
-        #Node to list of neighbors
-        self.nodes = {}
-
-    def nodes(self):
-        #Returns all the nodes of the graph
-        return self.nodes.keys()
+class Graph:
+    def __init__(self,dict=None):
+        if dict is None:
+            dict = {}
+        self.dict = dict
 
     def edges(self):
-        #Returns all the edges of a graph
+        return self.findedges()
+
+# Add the new edge
+    def AddEdge(self, edge):
+        edge = set(edge)
+        (vrtx1, vrtx2) = tuple(edge)
+        if vrtx1 in self.dict:
+            self.dict[vrtx1].append(vrtx2)
+        else:
+            self.dict[vrtx1] = [vrtx2]
+
+# List the edge names
+    def getEdges(self):
         edges = []
-        for node in self.nodes:
-            for num in self.nodes[node]:
-                edge = (node, num)
-                if edge not in edges:
-                    edges.append(edge)
+        for vrtx in self.dict:
+            for nxtvrtx in self.dict[vrtx]:
+                if {nxtvrtx, vrtx} not in edges:
+                    edges.append({vrtx, nxtvrtx})
         return edges
-
-    def insert_node(self, personId, commentId,text, timestamp, x):
-        #Insert a node which is a neighbor of x
-        node = Comment(personId,commentId,text , timestamp)
-        self.nodes[node] = [x]
-        self.nodes[x].append(node)
-
-    def remove_node(self, node):
-        #Removes a given node of the graph
-        for node in self.nodes:
-            if node in self.nodes[node]:
-                self.nodes[node].remove(node)
-        del self.nodes[node]
-
-
-    def print_graph(self):
-        print("my graph")
--- opioid-study/main.py
+++ opioid-study/main.py
@@ -1,6 +1,5 @@
 from core.graph import Graph
 from core.comment import Comment
-from core.person import Person
 import json
 
 json_data = json.load(open('data/opiates_data.json'))
@@ -10,6 +9,7 @@ myCommentGraph = Graph()
 for commentId, commentContent in json_data.items():
     #print(commentContent)
     #print(commentId)
+    a = Comment("tbd",commentId,commentContent[0]['1_text'],commentContent[0]['2_timestamp'])
     #print(commentContent[0]['0_title'])
     #print(commentContent[0]['1_text'])
     #print(commentContent[0]['2_timestamp'])
@@ -18,9 +18,11 @@ for commentId, commentContent in json_data.items():
             #print(userId)
             #print(subCommentContent[0].keys()) #all subComment id's
             for x in subCommentContent[0].items():
-                #a = Comment(userId,x[1][0]['1_text'],x[1][0]['0_timestamp'])
+                b = Comment(userId,x[0],x[1][0]['1_text'],x[1][0]['0_timestamp'])
+                myCommentGraph.AddEdge({a,b})
                 #myCommentGraph.insert_node(commentId,x[0], x[1][0]['0_timestamp'],)
                 #print(x[0]) #commentId
                 #print(x[1][0]['0_timestamp']) #timestamp
                 #print(x[1][0]['1_text']) #text
                 #myCommentGraph.insert_node(userId,x[0],x[1][0]['1_text'],x[1][0]['0_timestamp'],topmost comment?)
+    print(myCommentGraph.getEdges())
