4f3d051e15487c1cd1122cba63e3992d48c1df70
Brian <VanKrieg@users.noreply.github.com>
2018-04-10 15:02:44 -0500

    Add files via upload

--- opioid-study/main.py
+++ opioid-study/main.py
@@ -2,27 +2,48 @@ from core.graph import Graph
 from core.comment import Comment
 import json
 
-json_data = json.load(open('data/opiates_data.json'))
-
-myCommentGraph = Graph()
 ## do parsing data and convert to comment graph
-for commentId, commentContent in json_data.items():
-    #print(commentContent)
-    #print(commentId)
-    a = Comment("tbd",commentId,commentContent[0]['1_text'],commentContent[0]['2_timestamp'])
-    #print(commentContent[0]['0_title'])
-    #print(commentContent[0]['1_text'])
-    #print(commentContent[0]['2_timestamp'])
-    for subComment in (commentContent[0]['comments']):
-        for userId, subCommentContent in subComment.items():
-            #print(userId)
-            #print(subCommentContent[0].keys()) #all subComment id's
-            for x in subCommentContent[0].items():
-                b = Comment(userId,x[0],x[1][0]['1_text'],x[1][0]['0_timestamp'])
-                myCommentGraph.AddEdge({a,b})
-                #myCommentGraph.insert_node(commentId,x[0], x[1][0]['0_timestamp'],)
-                #print(x[0]) #commentId
-                #print(x[1][0]['0_timestamp']) #timestamp
-                #print(x[1][0]['1_text']) #text
-                #myCommentGraph.insert_node(userId,x[0],x[1][0]['1_text'],x[1][0]['0_timestamp'],topmost comment?)
-    print(myCommentGraph.getEdges())
+def parseJSON():
+    for group, topics in json_data.items():
+        #print(group)
+        for topicId, title in topics[0].items():
+            #print(topicId)
+            #print(title[0]['0_title'])
+            #print(title[0]['1_text'])
+            #print(title[0]['2_timestamp'])
+            for comment in title[0]['comments']:
+                for userId, comments in comment.items():
+                    if userId not in users:
+                        users.append(userId)
+                    #print(comments[0]['0_Comment ID'])
+                    #print(comments[0]['1_Text'])
+
+def makeRelationMatrix():
+    matrix = [[0 for x in range(0,len(users)+1)] for y in range(0,len(users)+1)]
+    for i in range(0,len(users)):
+        matrix[0][i+1] = users[i]
+        matrix[i+1][0] = users[i]
+
+    for group, topics in json_data.items():
+        for topicId, title in topics[0].items():
+            for comment in title[0]['comments']:
+                for userId, comments in comment.items():
+                    for i in range(0, len(users) + 1):
+                        if matrix[i][0] == userId:
+                            matrix[i][i] += 1
+    return matrix
+
+def printMatrix(matrix):
+    for i in range(0,len(users)+1):
+        for j in range(0,len(users)+1):
+            print (matrix[i][j] , end = " ")
+        print()
+
+json_data = json.load(open('data/opiates_data.json'))
+myCommentGraph = Graph()
+users = []
+parseJSON()
+users.sort()
+matrix = makeRelationMatrix()
+
+printMatrix(matrix)
