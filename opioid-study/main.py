659787c139806f096f1569d0a8ceeb08a9aad1b9
Brian <VanKrieg@users.noreply.github.com>
2018-04-22 19:30:24 -0500

    Add files via upload

--- opioid-study/main.py
+++ opioid-study/main.py
@@ -20,10 +20,10 @@ def parseJSON():
                     #print(comments[0]['1_Text'])
 
 def makeRelationMatrix():
-    matrix = [[0 for x in range(0,len(users)+1)] for y in range(0,len(users)+1)]
-    for i in range(0,len(users)):
-        matrix[0][i+1] = users[i]
-        matrix[i+1][0] = users[i]
+    matrix = [[0 for x in range(0, len(users) + 1)] for y in range(0, len(users) + 1)]
+    for i in range(0, len(users)):
+        matrix[0][i + 1] = users[i]
+        matrix[i + 1][0] = users[i]
 
     for group, topics in json_data.items():
         for topicId, title in topics[0].items():
@@ -31,18 +31,24 @@ def makeRelationMatrix():
                 topicAuthour = title[0]['3_author']
                 for userId, comments in comment.items():
                     commentAuthour = userId
+                    #print(topicAuthour)
+                    #print(commentAuthour)
                     for a in range(0, len(users)):
                         for b in range(0, len(users)):
                             if matrix[a][0] == topicAuthour and matrix[0][b] == commentAuthour:
+                                #print(matrix[a][0], matrix[0][b], matrix[a][b])
                                 matrix[a][b] += 1
-                            if matrix[a][0] == commentAuthour and matrix[0][b] == topicAuthour:
-                                matrix[a][b] += 1
+                                #print(matrix[a][0],matrix[0][b],matrix[a][b])
+                                #print(matrix[0][b], matrix[a][0], matrix[b][a])
+                                matrix[b][a] += 1
+                                #print( matrix[0][b],matrix[a][0], matrix[b][a])
+
     return matrix
 
 def printMatrix(matrix):
-    for i in range(0,len(users)+1):
-        for j in range(0,len(users)+1):
-            print (matrix[i][j] , end = " ")
+    for i in range(0,len(matrix)):
+        for j in range(0,len(matrix)):
+            print(matrix[i][j], end = " ")
         print()
 
 json_data = json.load(open('data/Addiction.json'))
@@ -50,6 +56,7 @@ myCommentGraph = Graph()
 users = []
 parseJSON()
 users.sort()
+
 matrix = makeRelationMatrix()
 
 printMatrix(matrix)
